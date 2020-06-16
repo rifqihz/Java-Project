66e0386df12471d01dc97aceedfac119cf5ea734
zhafrant <62896756+zhafrant@users.noreply.github.com>
2020-06-16 21:16:03 +0700

    Update Console_Input.java

--- Character Counting/Console_Input.java
+++ Character Counting/Console_Input.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package java_project.character_counting;
-
 public class Console_Input {
 
     private String consoleInput;
